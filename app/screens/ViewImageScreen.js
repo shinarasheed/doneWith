feca6c8c338399d4a9b98a0e3abbc1d4e8f73d7e
shina <rasheedshinaopeyemi@gmail.com>
2021-01-23 14:40:10 +0100

    added icons

--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Image, Text, View } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 import colors from '../config/colors';
 
